f81464cf63b0fb18ddcbd7f608137304f8b02581
Salfn1209 <85341765+Salfn1209@users.noreply.github.com>
2024-02-13 16:14:45 -0600

    Add files via upload

---
+++ js/jquery.js
@@ -0,0 +1,399 @@
+//Ocultar
+/*
+$(document).ready(function () {
+    $("#ocultarElemento").click(function () {
+        $(".wrapper1").hide(); // Oculta el elemento con la clase 'texto'
+    });
+});
+
+
+//Mostrar Elemento
+$(document).ready(function () {
+    $("#mostrarElemento").click(function () {
+        $(".wrapper1").show(); // Muestra el elemento con la clase 'card'
+    });
+});
+*/
+
+
+/*
+$(document).ready(function () {
+    $("#ocultarElemento2").click(function () {
+        $(".wrapper2").hide(); // Oculta el elemento con la clase 'texto'
+    });
+});
+
+
+//Mostrar Elemento
+$(document).ready(function () {
+    $("#mostrarElemento2").click(function () {
+        $(".wrapper2").show(); // Muestra el elemento con la clase 'card'
+    });
+});
+*/
+
+
+
+
+/*
+$(document).ready(function() {
+    // Ocultar elemento al cargar la página
+    $(".wrapper1").hide();
+
+    // Animación de desvanecimiento al hacer clic en "Ocultar Elemento"
+    $("#ocultarElemento").click(function() {
+        $(".wrapper1").fadeOut();
+    });
+
+    // Animación de desvanecimiento al hacer clic en "Mostrar Elemento"
+    $("#mostrarElemento").click(function() {
+        $(".wrapper1").fadeIn();
+    });
+});
+*/
+
+
+
+
+/* */
+$(document).ready(function () {
+    // Ocultar elemento al cargar la página
+    $(".wrapper1").hide();
+
+    // Animación personalizada al hacer clic en "Ocultar Elemento"
+    $("#ocultarElemento").click(function() {
+        $(".wrapper1").hide({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+
+    // Animación personalizada al hacer clic en "Mostrar Elemento"
+    $("#mostrarElemento").click(function() {
+        $(".wrapper1").show({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+});
+
+
+$(document).ready(function () {
+    // Ocultar elemento al cargar la página
+    $(".wrapper2").hide();
+
+    // Animación personalizada al hacer clic en "Ocultar Elemento"
+    $("#ocultarElemento2").click(function() {
+        $(".wrapper2").hide({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+
+    // Animación personalizada al hacer clic en "Mostrar Elemento"
+    $("#mostrarElemento2").click(function() {
+        $(".wrapper2").show({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+});
+
+
+$(document).ready(function () {
+    // Ocultar elemento al cargar la página
+    $(".wrapper3").hide();
+
+    // Animación personalizada al hacer clic en "Ocultar Elemento"
+    $("#ocultarElemento3").click(function() {
+        $(".wrapper3").hide({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+
+    // Animación personalizada al hacer clic en "Mostrar Elemento"
+    $("#mostrarElemento3").click(function() {
+        $(".wrapper3").show({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+});
+
+
+$(document).ready(function () {
+    // Ocultar elemento al cargar la página
+    $(".wrapper4").hide();
+
+    // Animación personalizada al hacer clic en "Ocultar Elemento"
+    $("#ocultarElemento4").click(function() {
+        $(".wrapper4").hide({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+
+    // Animación personalizada al hacer clic en "Mostrar Elemento"
+    $("#mostrarElemento4").click(function() {
+        $(".wrapper4").show({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+});
+
+
+$(document).ready(function () {
+    // Ocultar elemento al cargar la página
+    $(".wrapper5").hide();
+
+    // Animación personalizada al hacer clic en "Ocultar Elemento"
+    $("#ocultarElemento5").click(function() {
+        $(".wrapper5").hide({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+
+    // Animación personalizada al hacer clic en "Mostrar Elemento"
+    $("#mostrarElemento5").click(function() {
+        $(".wrapper5").show({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+});
+
+
+$(document).ready(function () {
+    // Ocultar elemento al cargar la página
+    $(".wrapper6").hide();
+
+    // Animación personalizada al hacer clic en "Ocultar Elemento"
+    $("#ocultarElemento6").click(function() {
+        $(".wrapper6").hide({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+
+    // Animación personalizada al hacer clic en "Mostrar Elemento"
+    $("#mostrarElemento6").click(function() {
+        $(".wrapper6").show({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+});
+
+
+$(document).ready(function () {
+    // Ocultar elemento al cargar la página
+    $(".wrapper7").hide();
+
+    // Animación personalizada al hacer clic en "Ocultar Elemento"
+    $("#ocultarElemento7").click(function() {
+        $(".wrapper7").hide({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+
+    // Animación personalizada al hacer clic en "Mostrar Elemento"
+    $("#mostrarElemento7").click(function() {
+        $(".wrapper7").show({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+});
+
+
+$(document).ready(function () {
+    // Ocultar elemento al cargar la página
+    $(".wrapper8").hide();
+
+    // Animación personalizada al hacer clic en "Ocultar Elemento"
+    $("#ocultarElemento8").click(function() {
+        $(".wrapper8").hide({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+
+    // Animación personalizada al hacer clic en "Mostrar Elemento"
+    $("#mostrarElemento8").click(function() {
+        $(".wrapper8").show({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+});
+
+$(document).ready(function () {
+    // Ocultar elemento al cargar la página
+    $(".wrapper9").hide();
+
+    // Animación personalizada al hacer clic en "Ocultar Elemento"
+    $("#ocultarElemento9").click(function() {
+        $(".wrapper9").hide({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+
+    // Animación personalizada al hacer clic en "Mostrar Elemento"
+    $("#mostrarElemento9").click(function() {
+        $(".wrapper9").show({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+});
+
+
+$(document).ready(function () {
+    // Ocultar elemento al cargar la página
+    $(".wrapper10").hide();
+
+    // Animación personalizada al hacer clic en "Ocultar Elemento"
+    $("#ocultarElemento10").click(function() {
+        $(".wrapper10").hide({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+
+    // Animación personalizada al hacer clic en "Mostrar Elemento"
+    $("#mostrarElemento10").click(function() {
+        $(".wrapper10").show({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+});
+
+
+$(document).ready(function () {
+    // Ocultar elemento al cargar la página
+    $(".wrapper11").hide();
+
+    // Animación personalizada al hacer clic en "Ocultar Elemento"
+    $("#ocultarElemento11").click(function() {
+        $(".wrapper11").hide({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+
+    // Animación personalizada al hacer clic en "Mostrar Elemento"
+    $("#mostrarElemento11").click(function() {
+        $(".wrapper11").show({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+});
+
+$(document).ready(function () {
+    // Ocultar elemento al cargar la página
+    $(".wrapper12").hide();
+
+    // Animación personalizada al hacer clic en "Ocultar Elemento"
+    $("#ocultarElemento12").click(function() {
+        $(".wrapper12").hide({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+
+    // Animación personalizada al hacer clic en "Mostrar Elemento"
+    $("#mostrarElemento12").click(function() {
+        $(".wrapper12").show({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+});
+
+
+
+/*
+$(document).ready(function() {
+    // Ocultar elemento al cargar la página
+    $(".wrapper2").hide();
+
+    // Animación personalizada al hacer clic en "Ocultar Elemento"
+    $("#ocultarElemento2").click(function() {
+        $(".wrapper2").animate({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+
+    // Animación personalizada al hacer clic en "Mostrar Elemento"
+    $("#mostrarElemento2").click(function() {
+        $(".wrapper2").show({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+});
+
+
+
+$(document).ready(function() {
+    // Ocultar elemento al cargar la página
+    $(".wrapper3").hide();
+
+    // Animación personalizada al hacer clic en "Ocultar Elemento"
+    $("#ocultarElemento3").click(function() {
+        $(".wrapper3").animate({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+
+    // Animación personalizada al hacer clic en "Mostrar Elemento"
+    $("#mostrarElemento3").click(function() {
+        $(".wrapper3").show({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+});
+
+
+$(document).ready(function() {
+    // Ocultar elemento al cargar la página
+    $(".wrapper4").hide();
+
+    // Animación personalizada al hacer clic en "Ocultar Elemento"
+    $("#ocultarElemento4").click(function() {
+        $(".wrapper4").animate({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+
+    // Animación personalizada al hacer clic en "Mostrar Elemento"
+    $("#mostrarElemento4").click(function() {
+        $(".wrapper4").show({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+});
+
+
+
+$(document).ready(function() {
+    // Ocultar elemento al cargar la página
+    $(".wrapper5").hide();
+
+    // Animación personalizada al hacer clic en "Ocultar Elemento"
+    $("#ocultarElemento5").click(function() {
+        $(".wrapper5").animate({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+
+    // Animación personalizada al hacer clic en "Mostrar Elemento"
+    $("#mostrarElemento5").click(function() {
+        $(".wrapper5").show({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+});
+
+
+$(document).ready(function() {
+    // Ocultar elemento al cargar la página
+    $(".wrapper6").hide();
+
+    // Animación personalizada al hacer clic en "Ocultar Elemento"
+    $("#ocultarElemento6").click(function() {
+        $(".wrapper6").animate({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+
+    // Animación personalizada al hacer clic en "Mostrar Elemento"
+    $("#mostrarElemento6").click(function() {
+        $(".wrapper6").show({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+});
+
+$(document).ready(function() {
+    // Ocultar elemento al cargar la página
+    $(".wrapper7").hide();
+
+    // Animación personalizada al hacer clic en "Ocultar Elemento"
+    $("#ocultarElemento7").click(function() {
+        $(".wrapper7").animate({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+
+    // Animación personalizada al hacer clic en "Mostrar Elemento"
+    $("#mostrarElemento7").click(function() {
+        $(".wrapper7").show({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+});
+
+$(document).ready(function() {
+    // Ocultar elemento al cargar la página
+    $(".wrapper8").hide();
+
+    // Animación personalizada al hacer clic en "Ocultar Elemento"
+    $("#ocultarElemento8").click(function() {
+        $(".wrapper8").animate({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+
+    // Animación personalizada al hacer clic en "Mostrar Elemento"
+    $("#mostrarElemento8").click(function() {
+        $(".wrapper8").show({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    });
+});
+
+
+*/
+
+/*
+$(document).ready(function() {
+    // Ocultar elemento al cargar la página
+    $(".wrapper2").hide();
+
+    // Animación de desvanecimiento al hacer clic en "Ocultar Elemento"
+    $("#ocultarElemento2").click(function() {
+        $(".wrapper2").fadeOut();
+    });
+
+    // Animación de desvanecimiento al hacer clic en "Mostrar Elemento"
+    $("#mostrarElemento2").click(function() {
+        $(".wrapper2").fadeIn();
+    });
+});
+*/
+
+
+/*
+$(document).ready(function () {
+    $("#ocultarElemento3").click(function () {
+        $(".menu").hide(); // Oculta el elemento con la clase 'texto'
+    });
+});
+
+
+//Mostrar Elemento
+$(document).ready(function () {
+    $("#mostrarElemento3").click(function () {
+        $(".menu").show(); // Muestra el elemento con la clase 'card'
+    });
+});
+*/
+
+
